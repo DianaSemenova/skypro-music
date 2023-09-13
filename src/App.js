8f29fa0bc14729879f12be7221462d9b5b9e0962
DianaSemenova <menestrelia199702@gmail.com>
2023-09-13 20:01:43 +0400

    добавлено перекл тумблера с помощью хука useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
+import { useState, useEffect } from "react";
 import { AudioPlayer } from "./components/AudioPlayer/AudioPlayer";
 import { NavMenu } from "./components/NavMenu/NavMenu";
 import { Sidebar } from "./components/Sidebar/Sidebar";
 import { TrackList } from "./components/TrackList/TrackList";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if (!loading) {
+      const timer = setTimeout(() => {
+        setLoading(true);
+      }, 10000);
+
+      
+      return () => clearTimeout(timer); 
+    }
+  }, [loading]);
+
+
   return (
     <div className="App">
       <div className="wrapper">
