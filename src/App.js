89762c137b42ab29e06def7cfbcb4bc64894cf11
DianaSemenova <menestrelia199702@gmail.com>
2023-09-29 22:53:16 +0400

    Создан маршрут на главную страницу Main

--- src/App.js
+++ src/App.js
@@ -1,40 +1,9 @@
-import { useState, useEffect } from "react";
-import * as S from './App.styles'
-import { AudioPlayer } from "./components/AudioPlayer/AudioPlayer";
-import { NavMenu } from "./components/NavMenu/NavMenu";
-import { Sidebar } from "./components/Sidebar/Sidebar";
-import { TrackList } from "./components/TrackList/TrackList";
-
+import { AppRoutes } from "./routes";
 
 function App() {
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    if (!isLoading) {
-      const timer = setTimeout(() => {
-        setLoading(true);
-      }, 5000);
-
-      return () => clearTimeout(timer);
-    }
-
-  }, [isLoading]);
-
   return (
-    <div className="App">
-      <S.wrapper>
-        <S.container>
-          <S.main>
-            <NavMenu />
-            <TrackList isLoading={isLoading} />
-            <Sidebar isLoading={isLoading} />
-          </S.main>
-          <AudioPlayer isLoading={isLoading}/>
-          <footer className="footer" />
-        </S.container>
-      </S.wrapper>
-    </div>
-  );
+    <AppRoutes />
+  )
 }
 
 export default App;
