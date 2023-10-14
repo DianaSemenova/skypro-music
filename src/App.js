7454651116fe7853892128ae9a9b1e62c8d47371
DianaSemenova <menestrelia199702@gmail.com>
2023-10-14 12:37:59 +0400

    SignIN/SignUP

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import { useState } from "react";
 import { AppRoutes } from "./components/routes/routes";
+// import { UserContext } from "./components/Context/Context";
 
 function App() {
-  const [user, setUser] = useState(false);
-  console.log(localStorage);
-  console.log(user);
+  const [user, setUser] = useState(localStorage.getItem("user") || null);
 
-  const handleSignIn = () => {
-    localStorage.setItem("user", "true");
-    const curentLocalStorage = localStorage.getItem("user");
-    console.log(curentLocalStorage);
-    setUser(curentLocalStorage);
-  };
+  // useEffect(() => {
+  //   setUser(localStorage.getItem("user") ? localStorage.getItem("user") : null);
+  //   console.log(localStorage.getItem("user"));
 
+  //   const currentIsLoginMode = localStorage.getItem("isLoginMode");
+  //   console.log(currentIsLoginMode);
+  // }, []);
+
+  
+
+  // const [user, setUser] = useState(false);
+  // console.log(localStorage);
+  // console.log(user);
+
+  // const handleSignIn = () => {
+  //   localStorage.setItem("user", "true");
+  //   const curentLocalStorage = localStorage.getItem("user");
+  //   console.log(curentLocalStorage);
+  //   setUser(curentLocalStorage);
+  // };
 
   // const handleSignUp = () => {
   //   localStorage.removeItem("user");
@@ -21,7 +33,12 @@ function App() {
   //   setUser(curentLocalStorage);
   // };
 
-  return <AppRoutes user={user} onAuthButtonClick={handleSignIn} />;
+  return (
+    <AppRoutes
+      user={user}
+      setUser={setUser}
+    />
+  );
 }
 
 export default App;
